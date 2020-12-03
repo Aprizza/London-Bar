826c969ef1dcbea5dcecc41fd0d09a4fd82b54d7
Aprizza <56188101+Aprizza@users.noreply.github.com>
2020-12-03 09:26:20 +0700

    Add files via upload

---
+++ data/LONDON_1.js
@@ -0,0 +1,8 @@
+var json_LONDON_1 = {
+"type": "FeatureCollection",
+"name": "LONDON_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ -0.113613064880465, 51.50481375271773 ] } }
+]
+}
